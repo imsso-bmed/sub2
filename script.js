5c056892d340f29cc8be867b489529cd84b5fb9d
imsso <syl942511@gmail.com>
2021-08-16 19:52:18 +0900

    0816

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(window).scroll(function(){
-    if(this.scrollY > 20){
-        $('nav').addClass("skicky");
-    }else{
-        $('nav').removeClass("skicky");
-    }
-  }
-})
