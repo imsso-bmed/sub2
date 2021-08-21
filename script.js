caf528486ee323ea64f45b8ba36de7f9d254eab1
imsso <syl942511@gmail.com>
2021-08-22 00:06:08 +0900

    0821

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Thyroid Gland", "내분비 기관", "목에 위치", "나비모양 기관"],
+        strings: ["내분비 기관", "목에 위치", "나비모양 기관"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
