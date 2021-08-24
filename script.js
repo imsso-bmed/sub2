1c3ee42a1c8f641cd277ef48c5fe745328b5dd94
imsso <syl942511@gmail.com>
2021-08-24 23:59:19 +0900

    0824

--- script.js
+++ script.js
@@ -66,24 +66,6 @@ $(document).ready(function(){
     });
 });
 
-// <!--스크롤 이미지 애니메이션-->
-
-$(document).ready(function() {
-    $(window).scroll( function(){
-			
-        $('image').each( function(i){
-            
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            if( bottom_of_window > bottom_of_element ){
-                $(this).animate({'opacity':'1'},700);
-            }
-            
-        }); 
-    });
-});
-
 
 $(document).on("scroll", function() {
     var pageTop = $(document).scrollTop();
