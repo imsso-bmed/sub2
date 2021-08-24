99a13e0ac79bbda8d151e0a9fcc80a5ad660716c
imsso <syl942511@gmail.com>
2021-08-24 23:57:55 +0900

    0824

--- script.js
+++ script.js
@@ -83,3 +83,21 @@ $(document).ready(function() {
         }); 
     });
 });
+
+
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+  
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });
+  
