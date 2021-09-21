1ed359295281f0ab5bc1a6924a6e48df5d95d97c
imsso <syl942511@gmail.com>
2021-09-22 01:41:12 +0900

    0922

--- thyroid-part2-desktop3.js
+++ thyroid-part2-desktop3.js
@@ -1,2 +1,88 @@
-b4w.register("thyroid-part2-desktop3_main",function(f,a){function g(a,b){b?(c.create_preloader(),a.oncontextmenu=function(a){a.preventDefault();a.stopPropagation();return!1},h.load(k+"thyroid-part2-desktop3.json",l,m)):console.log("b4w init failure")}function m(a){c.update_preloader(a)}function l(a,b){b?d.enable_camera_controls():console.log("b4w load failure")}var d=a("app"),n=a("config"),h=a("data"),c=a("preloader"),e="DEBUG"==a("version").type(),k=n.get_assets_path("thyroid-part2-desktop3");f.init=
-function(){d.init({canvas_container_id:"main_canvas_container",callback:g,show_fps:e,console_verbose:e,autoresize:!0})}});b4w.require("thyroid-part2-desktop3_main").init();
+"use strict"
+
+// register the application module
+b4w.register("thyroid-part2-desktop3_main", function(exports, require) {
+
+// import modules used by the app
+var m_app       = require("app");
+var m_cfg       = require("config");
+var m_data      = require("data");
+var m_preloader = require("preloader");
+var m_ver       = require("version");
+
+// detect application mode
+var DEBUG = (m_ver.type() == "DEBUG");
+
+// automatically detect assets path
+var APP_ASSETS_PATH = m_cfg.get_assets_path("thyroid-part2-desktop3");
+
+/**
+ * export the method to initialize the app (called at the bottom of this file)
+ */
+exports.init = function() {
+    m_app.init({
+        canvas_container_id: "main_canvas_container",
+        callback: init_cb,
+        show_fps: DEBUG,
+        console_verbose: DEBUG,
+        autoresize: true
+    });
+}
+
+/**
+ * callback executed when the app is initialized 
+ */
+function init_cb(canvas_elem, success) {
+
+    if (!success) {
+        console.log("b4w init failure");
+        return;
+    }
+
+    m_preloader.create_preloader();
+
+    // ignore right-click on the canvas element
+    canvas_elem.oncontextmenu = function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    };
+
+    load();
+}
+
+/**
+ * load the scene data
+ */
+function load() {
+    m_data.load(APP_ASSETS_PATH + "thyroid-part2-desktop3.json", load_cb, preloader_cb);
+}
+
+/**
+ * update the app's preloader
+ */
+function preloader_cb(percentage) {
+    m_preloader.update_preloader(percentage);
+}
+
+/**
+ * callback executed when the scene data is loaded
+ */
+function load_cb(data_id, success) {
+
+    if (!success) {
+        console.log("b4w load failure");
+        return;
+    }
+
+    m_app.enable_camera_controls();
+
+    // place your code here
+
+}
+
+
+});
+
+// import the app module and start the app by calling the init method
+b4w.require("thyroid-part2-desktop3_main").init();
